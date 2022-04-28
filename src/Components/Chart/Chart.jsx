a45d27218c627abe475586331262e254e24b6bda
0138876 <hajar.latigui@hotmail.com>
2022-04-28 05:30:37 +0200

    updated chart

--- src/Components/Chart/Chart.jsx
+++ src/Components/Chart/Chart.jsx
@@ -1,79 +1,69 @@
 import Plotly from "react-plotly.js";
-
+import { useParams } from "react-router-dom";
 
 const Chart = (props) => {
-		let trace_price = {
-			name: "Price ($)",
-			x: props.data.index.map((t) =>{
+
+    console.log('Chart.js');
+    console.log(props)
+    let { coinId } = useParams();
+
+    let trace_price = {
+            name: "Price (€)",
+            x: props.data.index.map((t) =>{
                 return(
                     new Date(t)
                 )
                 }),
-			y: props.data.price,
-			xaxis: "x",
-			yaxis: "y1",
-			type: "scatter",
-			mode: "lines+markers",
-			marker: { color: "blue", size: 3 },
-		};
-		let trace_volumes = {
-			name: "Volumne ($B)",
+            y: props.data.price,
+            marker: { color: "blue", size: 3 },
+        };
+        let trace_volumes = {
+			name: "Volume (€B)",
 			x: props.data.index.map((t) =>{
                 return(
                     new Date(t)
                 )
                 }),
 			y: props.data.volumes,
-			xaxis: "x",
-			yaxis: "y2",
-			type: "bar",
-			barmode: "relative",
 			marker: {
 				color: "rgb(49,130,189)",
 				opacity: 0.7,
 			},
 		};
-		let layout = {
-			autosize: true,
-			height: "100%",
-			margin: {
-				l: 50,
-				r: 20,
-				t: 35,
-				pad: 3,
-			},
-			showlegend: false,
-			xaxis: {
-				domain: [1, 1],
-				anchor: "y2",
-			},
-			yaxis: {
-				domain: [0.1, 1],
-				anchor: "x",
-			},
-			yaxis2: {
-				showticklabels: false,
-				domain: [0, 0.1],
-				anchor: "x",
-			},
-			grid: {
-				roworder: "bottom to top",
-			},
-		};
-		let config = { responsive: true };
-		let series = [trace_price, trace_volumes];
+        let layout = {
+            title: coinId,
+            autosize: false,
+            height: 500,
+            width: 1000
+        };
 
-        console.log(trace_price.y)
-        console.log("trace_price.y")
+        let lines = {
+        'price':{},
+        'volume':{}
+        }
+
+        let config = { responsive: true };
+        let series = [trace_price, trace_volumes];
+
+        console.log(series)
+        console.log(trace_volumes.x)        
+
         return( <Plotly
             data= {[{
                 x: trace_price.x,
                 y: trace_price.y,
-                type: 'scatter',
-                mode: 'lines+markers',
-                marker: {color: 'red'},
-            }]}
-            layout={layout}
+                type: 'scattergl',
+                marker:{color:"red"},
+                name: "Price"
+            },
+            {
+                x: trace_volumes.x,
+                y: trace_volumes.y,
+                type: 'scattergl',
+                marker:{color:"blue"},
+                name: "Volume"}
+            ]}
+            layout = {layout}
           />
           )
     }
--- src/Components/routes/Coin.jsx
+++ src/Components/routes/Coin.jsx
@@ -1,35 +1,41 @@
 import axios from 'axios';
-import React, { useEffect,} from 'react'
+import React, { useEffect, useState} from 'react'
 import {useParams } from 'react-router-dom';
 import Chart from '../Chart/Chart';
-
-const Coin = () => {
-let { coinId } = useParams();
-let data = { index: [], price: [], volumes: [] };
+import { useAsync } from "react-async"
 
 
+const loadData = async ({coinId}, {signal}) =>{
   const url = `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=usd&days=30&interval=daily`;
-  useEffect(()=> {
-      axios.get(url)
-      .then(response => {
-        for (const item of response.data.prices) {
-          data.index.push(item[0]);
-          data.price.push(item[1]);
-        }
-        for (const item of response.data.total_volumes) data.volumes.push(item[1]);
-    }).catch((error)=> {
-      console.log(error)
-    })
-  },[coinId])
-
-  return(
-    <>
-    <div className='chartDiv' id='chartDiv'>
-      <Chart data = {data} key = {data.id}/>
-    </div>
-       
-       </>
-)
+  const res = await fetch(url, { signal });
+  if (!res.ok) throw new Error(res.statusText)
+  return res.json();
 }
 
+
+const Coin = () => {
+  let { coinId } = useParams();
+
+  const { data, error, isPending } = useAsync({ promiseFn: loadData, coinId: coinId })
+
+  if (isPending) return "Loading..."
+  if (error) return `Something went wrong: ${error.message}`
+  if (data){
+    let factorizedData = { index: [], price: [], volumes: [] };
+    for(const item of data.prices){
+      factorizedData.index.push(item[0]);
+      factorizedData.price.push(item[1]);
+    }
+    for (const item of data.total_volumes){
+    factorizedData.volumes.push(item[1]);}
+
+    return(
+      <>
+      <div className='chartDiv' id='chartDiv'>
+        <Chart data = {factorizedData} key = {factorizedData.id}/>
+      </div>
+         </>)
+  } 
+  return null
+}
 export default Coin
