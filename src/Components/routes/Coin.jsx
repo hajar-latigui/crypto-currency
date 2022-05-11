50aa4142fd5c9e414f8993a57479efc814afa9ed
0138876 <0138876@users.noreply.github.com>
2022-05-11 08:56:49 +0200

    added typescript

--- src/Components/routes/Coin.jsx
+++ src/Components/routes/Coin.jsx
@@ -1,5 +1,4 @@
-import axios from 'axios';
-import React, { useEffect, useState} from 'react'
+import React from 'react'
 import {useParams } from 'react-router-dom';
 import Chart from '../Chart/Chart';
 import { useAsync } from "react-async"
